248005f77fa2dde5f405b89aeb64a1c98530cbec
Gleb <gleb.m.sokolov@gmail.com>
2017-02-07 21:54:09 -0500

    modified kinda final form

--- ch7/p7.py
+++ ch7/p7.py
@@ -1,20 +1,26 @@
-# import urllib.request
-from PIL import Image
+# challenge 7 at http://www.pythonchallenge.com/pc/def/oxygen.html 07/02/17
 
+from PIL import Image
+import re
+
+# import urllib.request
 # url = 'http://www.pythonchallenge.com/pc/def/oxygen.png'
 # urllib.request.urlretrieve(url, 'image.png')
 
 img = Image.open('image.png')
-
 width = img.width
 height = img.height
-pix = [[] for i in range(width)]
+pixels = [[] for i in range(width)]
+# pls rewrite this double loop python style
 for i in range(width):
     for j in range(height):
-        pix[i].append(img.getpixel((i, j)))
+        pixels[i].append(img.getpixel((i, j)))
 
-row = [el[48][1] for el in pix]
-print(row)
-out = [chr(el) for el in row]
-string = ''.join(out).split()
-print(string)
+row = [element[48][1] for element in pixels][::7]  # 48 is middle line with code encrypted in it, blocks of code each approx 7 pxls wide
+# print(row)
+
+# out = [chr(el) for el in row]
+message = ''.join(map(chr, row))
+nums = re.findall('\d+', message)
+result = ''.join(map(chr, map(int, nums)))
+print(result)
