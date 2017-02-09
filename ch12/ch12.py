82f6afab3f730df7d60122928637e59b7d4ab536
Gleb <gleb.m.sokolov@gmail.com>
2017-02-09 00:26:25 -0500

    totaly fail one

--- ch12/ch12.py
+++ ch12/ch12.py
@@ -1,12 +1,6 @@
-from PIL import Image
-import scipy.misc as smp
-img = Image.open('evil1.jpg')
+#challenge 12 http://www.pythonchallenge.com/pc/return/evil.html 9/2/17
 
-width = img.width
-height = img.height
+data = open('evil2.gfx','rb').read()
 
-pix = list(img.getdata())
-
-data = [[pix[i+j*width] for i in range(width)] for j in range(0,height,2)]
-image = smp.toimage(data)
-image.show()
+for i in range(5):
+    open(str(i)+'.jpg','wb').write(data[i::5])
